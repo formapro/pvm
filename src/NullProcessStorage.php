705b81d271d63a7225dd0cd6e01e016edd99edbe
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2018-02-23 11:58:47 +0200

    Fix NullProcessStorage should be compatible with StorageInterface

--- src/NullProcessStorage.php
+++ src/NullProcessStorage.php
@@ -6,15 +6,23 @@ class NullProcessStorage implements ProcessStorage
     /**
      * {@inheritdoc}
      */
-    public function persist(Process $process)
+    public function persist(Process $process): void
     {
     }
 
     /**
      * {@inheritdoc}
      */
-    public function get($id)
+    public function get(string $id): Process
     {
         throw new \LogicException(sprintf('The process with id "%s" could not be found', $id));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getByToken(string $token): Process
+    {
+        throw new \LogicException(sprintf('The process that has token "%s" could not be found', $token));
     }
 }
